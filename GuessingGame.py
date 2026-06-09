4d1231a0b6cf2d3bfa268dedb8b048a80aa4444e
Puneeth <punuveerapu24@gmail.com>
2026-06-09 20:05:59 +0530

    guessGame

--- GuessingGame.py
+++ GuessingGame.py
@@ -1,3 +1,33 @@
 
 import random
 
+lowest_number = 1
+highest_number = 100
+answer = random.randint(lowest_number, highest_number)
+guesses = 0
+is_running = True
+
+print("Welcome to the Guessing Game!")
+print(f"I'm thinking of a number between {lowest_number} and {highest_number}.")
+
+while is_running:
+    guess = input("Enter your guess: ")
+
+    if guess.isdigit():
+        guess = int(guess)
+        guesses += 1
+
+        if guess < lowest_number or guess > highest_number:
+            print("the number is out of range.")
+            print(f"Please enter a number between {lowest_number} and {highest_number}.")
+        elif guess < answer:
+            print("Too low! Try again.")
+        elif guess > answer:
+            print("Too high! Try again.")
+        else:
+            print(f"CORRECT! the answer is {answer}.")
+            print(f"You guessed the number in {guesses} guesses.")
+            is_running = False
+    else:
+        print("invalid")
+        print(f"Please enter a number between {lowest_number} and {highest_number}.")
